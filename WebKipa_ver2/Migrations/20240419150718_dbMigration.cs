e672a214ca07f07538a7750607bafaa6e57d65b2
unknown <vietanh912000@gmail.com>
2024-04-21 16:11:35 +0700

    update code

---
+++ WebKipa_ver2/Migrations/20240419150718_dbMigration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebKipa_ver2.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "banners",
+                columns: table => new
+                {
+                    BannerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_banners", x => x.BannerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "banners");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ WebKipa_ver2/Models/WebContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApp.Models
+{
+    public class WebContext : DbContext
+    {
+        public DbSet<Banner> banners { set; get; }        // bảng banner
+        public DbSet<User> users { set; get; }                // bảng user
+
+/*
+        public WebContext(DbContextOptions options) : base(options)
+        {
+
+        }*/
+        // chuỗi kết nối với tên db sẽ làm  việc đặt là webdb
+        public const string ConnectStrring = @"Data Source=VIETANHDEV\MSSQLSERVER22;Initial Catalog=KipaInfomation;User ID=sa;Password=123456;TrustServerCertificate=True";
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //var config = _appsettings.Value.DefaultConnection;
+            optionsBuilder.UseSqlServer(ConnectStrring);
+            //optionsBuilder.UseLoggerFactory(GetLoggerFactory());       // bật logger
+        }
+    }
+}
