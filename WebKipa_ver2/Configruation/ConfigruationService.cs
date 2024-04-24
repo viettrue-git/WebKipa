bd72023a5d1225877aa1135ad5c9b574b83fae19
TrangThiNguyen <Trang.nt@evotek.vn>
2024-04-25 06:54:36 +0700

    muntil langue

---
+++ WebKipa_ver2/Configruation/ConfigruationService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+namespace WebKipa_ver2.Configruation
+{
+    public class ConfigruationService
+    {
+        public static void RegisterGlobalizationAndLocalization(this IServiceCollection services)
+        {
+            var supportedCultures = new[]
+   {
+        new CultureInfo("vi-VI"),
+                new CultureInfo("korea-KOREA"),
+                // Add other cultures here
+    };
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+
+                options.DefaultRequestCulture = new RequestCulture("korea-KOREA");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+        }
+    }
+}
