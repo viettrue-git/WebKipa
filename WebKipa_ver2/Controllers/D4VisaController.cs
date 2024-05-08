17c529c6e16bd5eb089c3585c8829babe9cfb24d
TrangThiNguyen <Trang.nt@evotek.vn>
2024-05-08 23:51:56 +0700

    UI

---
+++ WebKipa_ver2/Controllers/D4VisaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebKipa_ver2.Controllers
+{
+    public class D4VisaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
